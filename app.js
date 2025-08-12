75aa77fee3a271498dc4b0b265336d8d94316a4f
MATHEUS GABRIEL PEREZ DOS SANTOS <25132972@CFP127ALUNO.LOCAL>
2025-08-12 16:58:12 -0300

    criação do js

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 'use strict'
 import funcionarios from './funcionarios.json' with {type:'json'}
 
-function funcionarios (){
+function mostrarFuncionarios (funcionarios){
+
+    const funcionariosContainer = document.getElementById('funcionarios')
+    const cards = document.getElementById('div')
+
+    const img = document.getElementById('img')
+    img.src = `./img/${funcionarios}`
+    img.classList.add('card-funcionario')
+
+    const nome = document.getElementById('h2')
 
     
+    const cargo = document.getElementById('p')
+    
+
+
 }
+
+funcionarios.forEach(mostrarFuncionarios)
+
