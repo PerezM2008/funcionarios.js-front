69363d81c33d07b0acb0d563c908df4fd4007931
MATHEUS GABRIEL PEREZ DOS SANTOS <25132972@CFP127ALUNO.LOCAL>
2025-08-19 15:44:05 -0300

    corrigir erro

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 'use strict'
+
 import funcionarios from './funcionarios.json' with {type:'json'}
 
-function mostrarFuncionarios (funcionarios){
+function mostrarFuncionarios (funcionario){
 
     const funcionariosContainer = document.getElementById('funcionarios')
-    const cards = document.getElementById('div')
+    const cards = document.createElement('div')
 
-    const img = document.getElementById('img')
-    img.src = `./img/${funcionarios}`
-    img.classList.add('card-funcionario')
+    const img = document.createElement('img')
+    img.src = `./img/${funcionario.img}`
+    img.alt = funcionario.nome
+    img.classList.add('funcionarios')
 
-    const nome = document.getElementById('h2')
+    const nome = document.createElement('h2')
+    nome.textContent = funcionario.nome
 
     
-    const cargo = document.getElementById('p')
-    
+    const cargo = document.createElement('p')
+    cargo.textContent = funcionario.cargo
 
-
+    cards.appendChild(img)
+    nome.appendChild(nome)
+    cargo.appendChild(cargo)
+    funcionariosContainer.appendChild(cards)
 }
 
+
 funcionarios.forEach(mostrarFuncionarios)
+console.log(funcionarios)
 
